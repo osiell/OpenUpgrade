133bf43f93b1c946c228bc5f1ff3ad7e4577646e
Rémi Hauperpin-Bonneau <remi.hauperpinbonneau@live.fr>
2017-03-23 15:35:26 +0100

    [FIX] project_mrp: migrate project_task.sale_line_id

--- addons/project_mrp/migrations/6.1.1.0/post-migrate.py
+++ addons/project_mrp/migrations/6.1.1.0/post-migrate.py
@@ -18,39 +18,26 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+import pooler
+
 from openerp.openupgrade import openupgrade
 
 
 @openupgrade.migrate()
 def migrate(cr, version):
-    """compute project.task's sale_line_id"""
-    cr.execute(
-        'select t.id, t.project_id, po.sale_id '
-        'from project_task t '
-        'join procurement_order po on t.procurement_id=po.id '
-        'where po.sale_id is not null')
-    for task_id, project_id, sale_id in cr.fetchall():
-        # we're looking for a sale line with the same project that is not
-        # yet claimed. That's the best matching we can do
-        cr.execute(
-            'update project_task set sale_line_id = '
-            '(select l.id from sale_order_line l '
-            'join product_product p on l.product_id=p.id '
-            'where '
-            'not exists (select id from project_task where sale_line_id=l.id) '
-            'and l.order_id=%s and p.project_id=%s limit 1) where id=%s',
-            (sale_id, project_id, task_id))
-        if project_id or cr.rowcount:
-            continue
-        # if this didn't work, don't restrict to project and use just any line
-        # with a service product
-        cr.execute(
-            'update project_task set sale_line_id = '
-            '(select l.id from sale_order_line l '
-            'left outer join product_product p on l.product_id=p.id '
-            'left outer join product_template t on p.product_tmpl_id=t.id '
-            'where '
-            'not exists (select id from project_task where sale_line_id=l.id) '
-            "and (l.product_id is null or t.type='service') "
-            'and l.order_id=%s limit 1) where id=%s',
-            (sale_id, task_id))
+    """Compute procurement.order's sale_line_id
+    then project.task's sale_line_id.
+    """
+    cr.execute("""
+        update procurement_order
+        set sale_line_id = so.id
+        from sale_order_line so
+        where so.procurement_id = procurement_order.id
+    """)
+    cr.execute("""
+        update project_task pt
+        set sale_line_id = po.sale_line_id
+        from procurement_order po
+        where po.id = pt.procurement_id
+        and po.sale_line_id is not null
+    """)
